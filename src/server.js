b16b104fdd195f14f05def9a5c5bb10564d1d12b
fmaganha <maganha@outlook.com>
2025-01-31 20:44:02 -0300

    Atualiza package.json para deploy na Vercel

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
 const pdfHandler = require('./pdfHandler');
 const fs = require('fs');
 
 const app = express();
 
-// Configuração do Multer para lidar com múltiplos arquivos
-const upload = multer({ dest: 'uploads/' }).fields([
-    { name: 'pdf', maxCount: 1 }, // Campo para o PDF
-    { name: 'rubrica', maxCount: 1 } // Campo para a rubrica
+// Configuração do Multer para usar memória em vez de disco
+const upload = multer({ storage: multer.memoryStorage() }).fields([
+    { name: 'pdf', maxCount: 1 },
+    { name: 'rubrica', maxCount: 1 }
 ]);
 
 app.use(express.static('public'));
@@ -22,22 +21,22 @@ app.post('/processar-pdf', upload, async (req, res) => {
         return res.status(400).send('PDF é obrigatório.');
     }
 
-    const pdfPath = req.files['pdf'][0].path; // Caminho do PDF
-    const rubricaPath = req.files['rubrica'] ? req.files['rubrica'][0].path : null; // Caminho da rubrica (se existir)
+    const pdfBuffer = req.files['pdf'][0].buffer; // Buffer do PDF
+    const rubricaBuffer = req.files['rubrica'] ? req.files['rubrica'][0].buffer : null; // Buffer da rubrica (se existir)
 
     // Validação da rubrica
-    if (incluirRubrica === 'on' && !rubricaPath) {
+    if (incluirRubrica === 'on' && !rubricaBuffer) {
         return res.status(400).send('Rubrica é obrigatória.');
     }
 
     try {
         const pdfPathProcessado = await pdfHandler.processarPDF(
-            pdfPath,
+            pdfBuffer, // Passa o buffer diretamente
             numeroInicial,
             numeroProcesso,
             anoProcesso,
-            incluirRubrica === 'on', // Converte para booleano
-            rubricaPath
+            incluirRubrica === 'on',
+            rubricaBuffer // Passa o buffer diretamente
         );
 
         res.download(pdfPathProcessado, 'documento_processado.pdf', (err) => {
@@ -45,9 +44,8 @@ app.post('/processar-pdf', upload, async (req, res) => {
                 console.error('Erro ao enviar o arquivo:', err);
                 res.status(500).send('Erro ao enviar o arquivo');
             } else {
-                // Exclui os arquivos temporários após o download
-                fs.unlinkSync(pdfPath);
-                if (rubricaPath) fs.unlinkSync(rubricaPath);
+                // Exclui o arquivo temporário após o download
+                fs.unlinkSync(pdfPathProcessado);
             }
         });
     } catch (error) {
