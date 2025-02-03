2497cd5c43138ffe33ae1f2308913f8925f104e6
fmaganha <maganha@outlook.com>
2025-02-03 20:45:30 -0300

    alteracoes_tamanho

--- pdfHandler.js
+++ pdfHandler.js
@@ -3,118 +3,116 @@ const fs = require('fs');
 const sharp = require('sharp');
 
 // Configurações globais
-const FONT_SIZE = 8;
 const RECT_MARGIN = 5;
-const RUBRICA_MAX_HEIGHT = 35;
 const RUBRICA_SCALE = 0.6;
 const TEXT_SPACING = 10;
 const RUBRICA_SPACING = 5;
-const MARGEM_RUBRICA_TEXTO = 5; // Nova margem entre o texto e a rubrica
+const MARGEM_RUBRICA_TEXTO = 2; // Margem entre o texto e a rubrica
 
 /**
- * Processa a rubrica, limitando a altura a 20px e mantendo a proporção.
+ * Processa a rubrica, limitando a altura a um valor máximo e mantendo a proporção.
  */
-async function processarRubrica(rubricaPath) {
-    try {
-        const rubricaBuffer = await sharp(rubricaPath)
-            .resize(null, RUBRICA_MAX_HEIGHT, { 
-                fit: 'inside',
-                withoutEnlargement: true,
-            })
-            .toBuffer();
-        return rubricaBuffer;
-    } catch (error) {
-        console.error('Erro ao processar a rubrica:', error);
-        throw new Error('Erro ao processar a rubrica: ' + error.message);
-    }
+async function processarRubrica(rubricaPath, rubricaHeight) {
+  try {
+    const rubricaBuffer = await sharp(rubricaPath)
+      .resize(null, parseInt(rubricaHeight), {
+        fit: 'inside',
+        withoutEnlargement: true,
+      })
+      .toBuffer();
+    return rubricaBuffer;
+  } catch (error) {
+    console.error('Erro ao processar a rubrica:', error);
+    throw new Error('Erro ao processar a rubrica: ' + error.message);
+  }
 }
 
 // Função para desenhar o texto e a rubrica no PDF
-async function processarPDF(pdfPath, numeroInicial, numeroProcesso, anoProcesso, incluirRubrica, rubricaPath) {
-    try {
-        const pdfBytes = fs.readFileSync(pdfPath);
-        const pdfDoc = await PDFDocument.load(pdfBytes);
-        const pages = pdfDoc.getPages();
-        const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+async function processarPDF(pdfPath, numeroInicial, numeroProcesso, anoProcesso, incluirRubrica, rubricaPath, fontSize, rubricaHeight) {
+  try {
+    const pdfBytes = fs.readFileSync(pdfPath);
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const pages = pdfDoc.getPages();
+    const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
 
-        let rubricaImage;
-        let rubricaWidth, rubricaHeight;
+    let rubricaImage;
+    let rubricaWidth, rubricaHeightFinal;
 
-        if (incluirRubrica && rubricaPath) {
-            const rubricaBuffer = await processarRubrica(rubricaPath);
-            const rubricaImageEmbed = await pdfDoc.embedPng(rubricaBuffer);
-            rubricaImage = rubricaImageEmbed;
-            rubricaWidth = rubricaImageEmbed.width * RUBRICA_SCALE;
-            rubricaHeight = rubricaImageEmbed.height * RUBRICA_SCALE;
-        }
-
-        let numeroFolha = parseInt(numeroInicial);
-
-        for (let i = 0; i < pages.length; i++) {
-            if (i % 2 === 0) {
-                const page = pages[i];
-                const { width, height } = page.getSize();
-
-                const paText = `PA: ${numeroProcesso}/${anoProcesso}`;
-                const flsText = `fls. ${numeroFolha}`;
-                const paTextWidth = font.widthOfTextAtSize(paText, FONT_SIZE);
-                const flsTextWidth = font.widthOfTextAtSize(flsText, FONT_SIZE);
-
-                const rectWidth = Math.max(paTextWidth, flsTextWidth, rubricaWidth || 0) + RECT_MARGIN * 2;
-                const textHeight = 15; 
-                const rectHeight = textHeight + (rubricaImage ? rubricaHeight + RUBRICA_SPACING : 0) + (rubricaImage ? RECT_MARGIN : RECT_MARGIN * 2);
-
-                const rectX = width - rectWidth - RECT_MARGIN;
-                const rectY = height - rectHeight - RECT_MARGIN;
-
-                page.drawRectangle({
-                    x: rectX,
-                    y: rectY,
-                    width: rectWidth,
-                    height: rectHeight,
-                    color: rgb(1, 1, 1),
-                    borderColor: rgb(1, 1, 1),
-                    borderWidth: 0,
-                });
-
-                page.drawText(paText, {
-                    x: rectX + rectWidth - paTextWidth - RECT_MARGIN,
-                    y: rectY + rectHeight - 10,
-                    size: FONT_SIZE,
-                    font,
-                    color: rgb(0, 0, 0),
-                });
-
-                page.drawText(flsText, {
-                    x: rectX + rectWidth - flsTextWidth - RECT_MARGIN,
-                    y: rectY + rectHeight - 10 - TEXT_SPACING,
-                    size: FONT_SIZE,
-                    font,
-                    color: rgb(0, 0, 0),
-                });
-
-                if (rubricaImage) {
-                    page.drawImage(rubricaImage, {
-                        x: rectX + rectWidth - rubricaWidth - RECT_MARGIN,
-                        y: rectY + RECT_MARGIN - MARGEM_RUBRICA_TEXTO, // Adiciona a margem acima da rubrica
-                        width: rubricaWidth,
-                        height: rubricaHeight,
-                    });
-                }
-
-                numeroFolha++;
-            }
-        }
-
-        const outputPath = `output_${Date.now()}.pdf`;
-        const pdfBytesProcessado = await pdfDoc.save();
-        fs.writeFileSync(outputPath, pdfBytesProcessado);
-
-        return outputPath;
-    } catch (error) {
-        console.error('Erro ao processar o PDF:', error);
-        throw new Error('Erro ao processar o PDF: ' + error.message);
+    if (incluirRubrica && rubricaPath) {
+      const rubricaBuffer = await processarRubrica(rubricaPath, rubricaHeight);
+      const rubricaImageEmbed = await pdfDoc.embedPng(rubricaBuffer);
+      rubricaImage = rubricaImageEmbed;
+      rubricaWidth = rubricaImageEmbed.width * RUBRICA_SCALE;
+      rubricaHeightFinal = rubricaImageEmbed.height * RUBRICA_SCALE;
     }
+
+    let numeroFolha = parseInt(numeroInicial);
+
+    for (let i = 0; i < pages.length; i++) {
+      if (i % 2 === 0) {
+        const page = pages[i];
+        const { width, height } = page.getSize();
+
+        const paText = `PA: ${numeroProcesso}/${anoProcesso}`;
+        const flsText = `fls. ${numeroFolha}`;
+        const paTextWidth = font.widthOfTextAtSize(paText, parseInt(fontSize));
+        const flsTextWidth = font.widthOfTextAtSize(flsText, parseInt(fontSize));
+
+        const rectWidth = Math.max(paTextWidth, flsTextWidth, rubricaWidth || 0) + RECT_MARGIN * 2;
+        const textHeight = 15;
+        const rectHeight = textHeight + (rubricaImage ? rubricaHeightFinal + RUBRICA_SPACING : 0) + (rubricaImage ? RECT_MARGIN : RECT_MARGIN * 2);
+
+        const rectX = width - rectWidth - RECT_MARGIN;
+        const rectY = height - rectHeight - RECT_MARGIN;
+
+        page.drawRectangle({
+          x: rectX,
+          y: rectY,
+          width: rectWidth,
+          height: rectHeight,
+          color: rgb(1, 1, 1),
+          borderColor: rgb(1, 1, 1),
+          borderWidth: 0,
+        });
+
+        page.drawText(paText, {
+          x: rectX + rectWidth - paTextWidth - RECT_MARGIN,
+          y: rectY + rectHeight - 10,
+          size: parseInt(fontSize),
+          font,
+          color: rgb(0, 0, 0),
+        });
+
+        page.drawText(flsText, {
+          x: rectX + rectWidth - flsTextWidth - RECT_MARGIN,
+          y: rectY + rectHeight - 10 - TEXT_SPACING,
+          size: parseInt(fontSize),
+          font,
+          color: rgb(0, 0, 0),
+        });
+
+        if (rubricaImage) {
+          page.drawImage(rubricaImage, {
+            x: rectX + rectWidth - rubricaWidth - RECT_MARGIN,
+            y: rectY + RECT_MARGIN - MARGEM_RUBRICA_TEXTO,
+            width: rubricaWidth,
+            height: rubricaHeightFinal,
+          });
+        }
+
+        numeroFolha++;
+      }
+    }
+
+    const outputPath = `output_${Date.now()}.pdf`;
+    const pdfBytesProcessado = await pdfDoc.save();
+    fs.writeFileSync(outputPath, pdfBytesProcessado);
+
+    return outputPath;
+  } catch (error) {
+    console.error('Erro ao processar o PDF:', error);
+    throw new Error('Erro ao processar o PDF: ' + error.message);
+  }
 }
 
 module.exports = { processarPDF };
--- server.js
+++ server.js
@@ -23,44 +23,46 @@ if (!fs.existsSync(uploadDir)) {
 app.use(express.static('public'));
 
 app.post('/processar-pdf', upload, async (req, res) => {
-    const { numeroInicial, numeroProcesso, anoProcesso, incluirRubrica } = req.body;
-
+    const { numeroInicial, numeroProcesso, anoProcesso, incluirRubrica, fontSize, rubricaHeight } = req.body;
+  
     // Verifica se o PDF foi enviado
     if (!req.files['pdf']) {
-        return res.status(400).send('PDF é obrigatório.');
+      return res.status(400).send('PDF é obrigatório.');
     }
-
+  
     const pdfPath = req.files['pdf'][0].path; // Caminho do PDF
     const rubricaPath = req.files['rubrica'] ? req.files['rubrica'][0].path : null; // Caminho da rubrica (se existir)
-
+  
     // Validação da rubrica
     if (incluirRubrica === 'on' && !rubricaPath) {
-        return res.status(400).send('Rubrica é obrigatória.');
+      return res.status(400).send('Rubrica é obrigatória.');
     }
-
+  
     try {
-        console.log('Iniciando processamento do PDF...');
-        const pdfPathProcessado = await pdfHandler.processarPDF(
-            pdfPath,
-            numeroInicial,
-            numeroProcesso,
-            anoProcesso,
-            incluirRubrica === 'on', // Converte para booleano
-            rubricaPath
-        );
-        console.log('PDF processado com sucesso!', pdfPathProcessado);
-
-        // Devolver caminho para o cliente para o download
-        res.send(`/download/${path.basename(pdfPathProcessado)}`);
-
-        // Exclui os arquivos temporários após o download
-        fs.unlinkSync(pdfPath);
-        if (rubricaPath) fs.unlinkSync(rubricaPath);
+      console.log('Iniciando processamento do PDF...');
+      const pdfPathProcessado = await pdfHandler.processarPDF(
+        pdfPath,
+        numeroInicial,
+        numeroProcesso,
+        anoProcesso,
+        incluirRubrica === 'on', // Converte para booleano
+        rubricaPath,
+        fontSize, // Novo parâmetro
+        rubricaHeight // Novo parâmetro
+      );
+      console.log('PDF processado com sucesso!', pdfPathProcessado);
+  
+      // Devolver caminho para o cliente para o download
+      res.send(`/download/${path.basename(pdfPathProcessado)}`);
+  
+      // Exclui os arquivos temporários após o download
+      fs.unlinkSync(pdfPath);
+      if (rubricaPath) fs.unlinkSync(rubricaPath);
     } catch (error) {
-        console.error('Erro ao processar o PDF:', error);
-        res.status(500).send(`Erro ao processar o PDF: ${error.message}`);
+      console.error('Erro ao processar o PDF:', error);
+      res.status(500).send(`Erro ao processar o PDF: ${error.message}`);
     }
-});
+  });
 
 app.get('/download/:filename', (req, res) => {
     const filePath = path.join(__dirname, req.params.filename);
